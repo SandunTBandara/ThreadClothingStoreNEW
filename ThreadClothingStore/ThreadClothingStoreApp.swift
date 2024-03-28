219ca90bc7f94a12dd89d4d8a8fdc290fe20875b
SandunTBandara <devocksandu99@gmail.com>
2024-03-28 16:32:17 +0530

    Initial Commit

---
+++ ThreadClothingStore/ThreadClothingStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadClothingStoreApp.swift
+//  ThreadClothingStore
+//
+//  Created by Sandun Bandara on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct ThreadClothingStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadClothingStore/View/Login/WelcomeView.swift
@@ -0,0 +1,70 @@
+//
+//  WelcomeView.swift
+//  ThreadClothingStore
+//
+//  Created by Sandun Bandara on 2024-03-28.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack{
+            Image("femalebuy")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .screenWidth, height: .screenHeight)
+            
+            VStack{
+                Spacer()
+                
+                Image("logobrand")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                    .padding(.bottom, 8)
+                
+                Text( "Welcome\nto our store")
+                    .font(.customfont(.semibold, fontSize: 48))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text( "Ger your groceries in as fast as one hour")
+                    .font(.customfont(.medium, fontSize: 16))
+                    .foregroundColor(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                
+                NavigationLink {
+                    SignInView()
+                } label: {
+                    RoundButton(title: "Get Started") {
+                    }
+                }
+
+               
+                
+                Spacer()
+                    .frame(height: 80)
+                
+            }
+            .padding(.horizontal , 20)
+        }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea()
+    }
+        
+    
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        NavigationView {
+            WelcomeView()
+        }
+        
+    }
+}
